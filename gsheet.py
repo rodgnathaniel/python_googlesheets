f45ef48a8c0e55170a08f43754aa729b8ba68a02
rodgnathaniel <rodgnathaniel@gmail.com>
2019-05-01 09:46:07 -0700

    fixed typo in file name

--- gsheet.py
+++ gsheet.py
@@ -2,7 +2,7 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
 scope = ['https://spreadsheets.google.com/feeds']
-creds = ServiceAccountCredentials.from_json_keyfile_name('cliend_secret.json', scope)
+creds = ServiceAccountCredentials.from_json_keyfile_name('client.json', scope)
 
 gsheet = client.open('test').sheet1
 
