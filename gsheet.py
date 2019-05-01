caa00ef9d36967dd2e4ba40d22bab552174ca30c
rodgnathaniel <rodgnathaniel@gmail.com>
2019-05-01 09:41:08 -0700

    \

--- gsheet.py
+++ gsheet.py
@@ -2,4 +2,9 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
 scope = ['https://spreadsheets.google.com/feeds']
-creds = ServiceAccountCredentials
+creds = ServiceAccountCredentials.from_json_keyfile_name('cliend_secret.json', scope)
+
+gsheet = client.open('test').sheet1
+
+test = gsheet.get_all_records()
+print(test)
